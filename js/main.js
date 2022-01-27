ad57a40ad21856272b0791f5fed0fee96fa4842b
Pedro Marins <contato@pedromarins.com>
2022-01-26 23:43:51 -0300

    video 4.2

--- js/main.js
+++ js/main.js
@@ -23,6 +23,8 @@ form.addEventListener("submit", (evento) => {
         itemAtual.id = existe.id
         
         atualizaElemento(itemAtual)
+
+        itens[existe.id] = itemAtual
     } else {
         itemAtual.id = itens.length
 
